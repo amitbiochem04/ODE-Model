40947f24a0dc2c9d397328fad545e68fbd22b8ac
amitbiochem04@gmail.com <amitbiochem04@gmail.com>
2017-05-11 14:46:58 +0200

    creat

---
+++ parameter_estimation.m
@@ -0,0 +1,18 @@
+function x=estimate()
+    
+
+
+k0=[1.2736 0.1000 0.1000 0.1106 8.7757 0.3736 3.2675];
+
+
+
+options=optimset('LevenbergMarquardt','on','LargeScale','on','TolX',10^(-12),'MaxFunEvals',10^12,'MaxIter',10^4,'Display','on','TolFun',10^(-12));
+
+[x,resnorm,residual,exitflag,output]=lsqnonlin(@cost,k0,[0.01 0.01 0.01 0.01 0.01 0.01 0.01],[20 20 20 20 20 20 20],options)
+
+%Jacobian = full(Jacobian); 
+%varp = resnorm*inv(Jacobian'*Jacobian)/length(50);
+    				%stdp = sqrt(diag(varp));
+					%AIC=log(residual /50)+2*23;
+					
+return
---
+++ senestivity_analaysis.m
@@ -0,0 +1,3 @@
+
+[t y] = sensitivity('amit', [0 1], [ 1 3 3 0 0 0 0 2 8], 5,  0.05);
+surf(y);
